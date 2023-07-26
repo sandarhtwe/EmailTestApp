83a1ca37df43baea2ed1a407feb4d58450e85a82
WaiWaiKhaing <dev@quantummsolutions.com>
2023-07-26 20:38:51 +0630

    Add project files.

---
+++ EmailTestApp/Controllers/EmailController.cs
@@ -0,0 +1,33 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace EmailTestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email=new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("sandarhtwe.sdh16@gmail.com"));
+            email.To.Add(MailboxAddress.Parse("sandarhtwe@acedatasystems.com"));
+            email.Subject="Test Email Service";
+            email.Body=new TextPart(TextFormat.Html) { Text=body};
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("sandarhtwe.sdh16@gmail.com", "twgantpdeortakmi");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+
+        }
+    }
+}
---
+++ EmailTestApp/Controllers/PdfGeneratorController.cs
@@ -0,0 +1,71 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Net.Mail;
+
+namespace EmailTestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfGeneratorController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GeneratePdfAndSendEmail([FromBody] string email)
+        {
+            try
+            {
+                
+                SendEmailWithAttachment(email, "Product List Report");
+
+                return Ok("PDF generated and sent to the email.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error occurred: " + ex.Message);
+            }
+        }
+
+
+        private void SendEmailWithAttachment(string recipientEmail, string subject)
+        {
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Sandar Htwe", "sandarhtwe.sdh16@gmail.com"));
+            message.To.Add(new MailboxAddress("", recipientEmail));
+            message.Subject = subject;
+
+            // Adding the mail body using a TextPart
+            var textPart = new TextPart("plain")
+            {
+                Text = "Dear Sir,\r\n\r\nThis is the report of Product List. Please kindly check it. Thanks Sir.\r\n\r\nBest Regards,\r\n\r\nSandar"
+            };
+
+            // Adding the attachment using a BodyBuilder
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.Attachments.Add("ProductList.pdf");
+
+            // Set the text body and attachment body to the MimeMessage
+            var multipart = new Multipart("mixed");
+            multipart.Add(textPart);
+            multipart.Add(bodyBuilder.ToMessageBody());
+            message.Body = multipart;
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                // Replace with your SMTP server address and port number
+                client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+
+                // Provide your authentication credentials 
+                client.Authenticate("sandarhtwe.sdh16@gmail.com", "twgantpdeortakmi");
+
+                // Send the email
+                client.Send(message);
+
+                // Disconnect from the SMTP server
+                client.Disconnect(true);
+            }
+        }
+    }
+}
